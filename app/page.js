a93cc50a9f1d378ec8661e28afc4b1f01875b20a
johnniedom <johnniedom1@gmail.com>
2025-04-14 22:20:00 +0100

    commit today

--- app/page.js
+++ app/page.js
@@ -3,25 +3,28 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="mx-auto grid min-h-screen max-w-screen-xl place-items-center gap-12 px-[1rem] py-[4rem] xl:grid-cols-2 xl:gap-0">
-      <div className="grid max-w-md grid-cols-1 place-items-center gap-4 sm:gap-6 xl:col-start-2 xl:col-end-3 xl:justify-items-start xl:my-auto" >
+      <div className="grid max-w-md grid-cols-1 place-items-center gap-4 sm:gap-6 xl:col-start-2 xl:col-end-3 xl:my-auto xl:justify-items-start">
         <EpicStackLogo className="size-20" />
         <h1 className="md:text-5xl text-center text-4xl font-bold sm:text-4.5xl lg:text-5.5xl">
           The <span className="text-highlight">Epic</span> Stack
         </h1>
-        <p className="md:2xl w-full text-center text-base font-thin leading-6 sm:text-lg">
+        <p className="md:2xl w-full text-center text-base font-thin leading-6 sm:text-lg xl:text-start">
           Check the{' '}
-          <a href="#" className="font-bold underline hover:underline-offset-0">
+          <a
+            href="#"
+            className="Button A font-bold underline hover:outline hover:outline-2 hover:outline-highlight"
+          >
             Getting Started
           </a>{' '}
           guide file for how to get your project off the ground!
         </p>
       </div>
       <div className="xl:col-start-1 xl:col-end-2 xl:row-start-1 xl:row-end-2">
-        <ul className="flex max-w-[840px] flex-wrap place-content-center gap-2 xl:gap-4 p-2 xl:grid grid-rows-6 grid-cols-5 ">
+        <ul className="flex max-w-[840px] grid-cols-5 grid-rows-6 flex-wrap place-content-center gap-2 p-2 xl:grid xl:gap-4">
           {logos.map((logo) => (
             <li
               key={logo.href}
-              className={`flex size-24 items-center justify-center rounded-xl bg-wrapper p-4 ${logo.col} ${logo.row} `}
+              className={`flex size-24 active:translate-x-6 items-center justify-center rounded-xl bg-wrapper p-4 ${logo.col} ${logo.row} focus:outline focus:hover:outline-3 focus:hover:outline-highlight`}
             >
               <a href={logo.href}>
                 <Image
